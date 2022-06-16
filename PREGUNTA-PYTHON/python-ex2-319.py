f2c28db0905678758d0b5377c24e2ba94bb831cc
titan0000001 <105446302+titan0000001@users.noreply.github.com>
2022-06-15 21:59:34 -0400

    Update python-ex2-319.py

--- PREGUNTA-PYTHON/python-ex2-319.py
+++ PREGUNTA-PYTHON/python-ex2-319.py
@@ -5,7 +5,7 @@ Created on Sat Jun 11 18:40:11 2022
 @author: YURIKA MACUSAYA AGUILAR 
 """
 #PREGUNTA 1, PYTHON
-# Python program to display the Fibonacci sequene from 1st fibonaccio to N fibonacci
+#Realice la serie Fibonacci estructurada y recursiva de tres términos hasta n en f#, haskell, scala y Python.
 # 3 terms until  N = 7 
 def estructurado_fibo(a=-1, b=1, f=0, n=0):        
     while n!=0:
@@ -21,7 +21,20 @@ def recur_fibo(a, b, f, n):
         print(f, end=',')
         recur_fibo(a, b, f, n-1)
 
+        
+        
 #PREGUNTA 2, PYTHON
+#Realice la serie Fibonacci estructurada y recursiva de tres términos hasta n en f#, haskell, scala y Python.
+#lambda
+print('\nusando lambda' )
+from functools import reduce
+fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])   
+print(fib(7)) 
+
+
+
+#PREGUNTA 3, PYTHON
+#Resuelva el ejercicio 1 utilizando funciones de orden superior (Haskell, scala, python).
 #funciones de order superior
 def funciones(funcion,d =0):
     return funcion(-1, 1, 0, d)
@@ -32,10 +45,4 @@ funciones(estructurado_fibo, 7)
 print('\nrecursivo' )
 funciones(recur_fibo, 7)
 
-#PREGUNTA 3, PYTHON
-#lambda
-print('\nusando lambda' )
-from functools import reduce
-fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])   
-print(fib(7)) 
-#enterprise java beans
+
