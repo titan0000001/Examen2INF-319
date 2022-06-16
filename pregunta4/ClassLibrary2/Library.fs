7ba04da9daa85763aef8fc15db1b912db84dc455
titan0000001 <105446302+titan0000001@users.noreply.github.com>
2022-06-15 23:14:56 -0400

    Add files via upload

---
+++ pregunta4/ClassLibrary2/Library.fs
@@ -0,0 +1,48 @@
+﻿namespace ClassLibrary2
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+// UNIV. MACUSAYA AGUILAR YURIKA CI:13846755 LP
+
+module pregunta4 =
+let fiboIterativo n = 
+    match n with 
+    | 0 -> 0
+    | n -> 
+        let mutable last = 0
+        let mutable next = 1
+        for i in 1 .. (n - 1) do
+            let temp = last + next
+            last <- next
+            next <- temp
+            printfn "%i" temp
+        next
+//FIBONACCI RECURSIVO
+let rec fib4(n: int):int = 
+    match n with
+    | 1 | 2 -> n  
+    | n -> fib4 (n-1) + fib4 (n - 2)
+
+let function1(n: int) =
+  for i in 1 .. n do
+    fib4 i |> printfn "%i" |> ignore
+
+
+
+[<EntryPoint>]
+let main argv =    
+    // PREGUNTA 1
+    //Realice la serie Fibonacci estructurada y recursiva de tres términos hasta n 
+    printf "%s" "FIBONACCI ITERATIVO " 
+    //iterativo
+    fiboIterativo 7 
+    printf "%s " "FIBONACCI RECURSIVO " 
+    //recursivo
+    function1(7)
+    0
+    
---
+++ pregunta4/WebApplicationPregunta4319 - copia/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationPregunta4319.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
